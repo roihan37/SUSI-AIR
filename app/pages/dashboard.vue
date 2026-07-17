b37a052877cc3d6dca4e1e692b97b14b7d3466c5
Amir Amilin <amiramilin@MacBook-Pro-Amir.local>
2026-07-17 14:35:06 +0700

    fix:news

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,173 +1,125 @@
 <script setup lang="ts">
 import FlightSummary from '~/components/dashboard/FlightSummary.vue';
-import UpComingFlight from '~/components/dashboard/UpComingFlight.vue';
 import HoursToLimit from '~/components/dashboard/HoursToLimit.vue';
 import MyDocuments from '~/components/dashboard/MyDocuments.vue';
-import { useDashboard } from '~/composables/useDashboard';
-import News from '~/components/dashboard/News.vue';
+import UpComingFlight from '~/components/dashboard/UpComingFlight.vue';
 
+const openSidebar = useState('sidebar-open')
+function toggleSidebar() {
+    openSidebar.value = !openSidebar.value
+}
 const { cards } = useFlightLimit()
-
-const {
-    pilot,
-    todaySummary,
-    upcomingFlight,
-    news,
-    documentList
-} = useDashboard()
-
+const { pilot } = useFlightHoursStore()
+const { todaySummary } = useScheduleStore()
+const { upcomingFlight } = useUpcomingFlightStore()
+const newsStore = useNewsStore()
+const documentStore = useDocumentStore()
 const formattedDate = computed(() => {
-    if (!upcomingFlight.value?.dutyDate) return ""
+    if (!upcomingFlight?.dutyDate) return ""
 
-    return new Date(upcomingFlight.value.dutyDate).toLocaleDateString("en-GB", {
+    return new Date(upcomingFlight.dutyDate).toLocaleDateString("en-GB", {
         day: "numeric",
         month: "short",
-        year: "numeric"
+        year: "numeric",
     })
 })
-
 
 
 
 </script>
 
 <template>
-   
-    
-        <!-- <pre>{{ latestNews }}</pre> -->
-        <Headers :title="'Captain ' + pilot.name" :subTilte="`Have a save and productive flight`" />
-        <div class=" bg-zinc-50 grid grid-col-1 gap-5">
-            <!-- HEADERS -->
-    
-            <!-- SUMMAERY -->
-            <FlightSummary v-if="pilot" :name="pilot.totalFlightHours" :base="todaySummary?.base" />
-    
-            <!-- UPCOMING FLIGHT -->
-            <div class="grid grid-cols-1 gap-5 sm:grid-cols-2">
-                <UpComingFlight :flight="upcomingFlight" :formatted-date="formattedDate" />
-                <!-- BAGIAN NEWS -->
-    
-    
-                <template>
-                    <UCard class="rounded-3xl p-5 shadow-md">
+    <div class=" bg-zinc-50 grid grid-col-1 gap-5">
+        <!-- HEADERS -->
+       <Headers 
+       :title="'Captain ' + pilot.name"
+       :subTilte="`Have a save and productive flight`"
+        />
 
-    <!-- Header -->
-    <div class="mb-6 flex items-center justify-between">
+        <!-- SUMMAERY -->
+        <FlightSummary
+        v-if="pilot"
+        :name="pilot.totalFlightHours"
+        :base="todaySummary?.base"
+        />
 
-      <h2 class="text-2xl font-bold">
+        <!-- UPCOMING FLIGHT -->
+        <div class="grid grid-cols-1 gap-5 sm:grid-cols-2">
+            <UpComingFlight
+                :flight="upcomingFlight"
+                :formatted-date="formattedDate"
+            />
+            <!-- BAGIAN NEWS -->
+            <UCard class="rounded-3xl p-5 shadow-md">
 
-        Latest News
+                <!-- Header -->
+                <div class="mb-6 flex items-center justify-between">
+                    <h2 class="text-2xl font-bold">
+                        Latest News
+                    </h2>
 
-      </h2>
+                    <UButton variant="ghost" color="error">
+                        View all
+                    </UButton>
+                </div>
 
-      <UButton
-        variant="ghost"
-        color="error"
-      >
+                <!-- Scroll -->
+                <div class="overflow-x-auto overflow-y-hidden no-scrollbar">
+                    <div class="flex w-max gap-5">
 
-        View all
+                        <div v-for="item in newsStore.latestNews" :key="item.id"
+                            class="w-[270px] overflow-hidden rounded-2xl p-0 shadow-lg my-2 ">
 
-      </UButton>
+                            <NuxtImg :src="item.image" :alt="item.title" class="h-44 w-full object-cover rounded-2xl"/>
 
-    </div>
+                            <div class="p-4">
 
-    <!-- News List -->
+                                <UBadge variant="soft" color="neutral" class="rounded-full">
+                                    {{ item.category }}
+                                </UBadge>
 
-    <div
-      class="overflow-x-auto overflow-y-hidden no-scrollbar"
-    >
+                                <h3 class="mt-4 text-xl font-bold">
+                                    {{ item.title }}
+                                </h3>
 
-      <div
-        class="flex w-max gap-5"
-      >
+                                <p class="mt-3 text-sm text-gray-500">
+                                    {{ item.time }}
+                                </p>
 
-        <article
-          v-for="item in news"
-          :key="item.id"
-          class="
-            my-2
-            w-[270px]
-            overflow-hidden
-            rounded-2xl
-            bg-white
-            shadow-lg
-            transition
-            hover:-translate-y-1
-            hover:shadow-xl
-          "
-        >
+                            </div>
 
-          <img
-           
-            :alt="item.title"
-            class="h-44 w-full rounded-t-2xl object-cover"
-          />
+                        </div>
 
-          <div class="p-4">
+                    </div>
+                </div>
 
-            <UBadge
-              variant="soft"
-              color="neutral"
-              class="rounded-full"
-            >
-
-              {{ item.category }}
-
-            </UBadge>
-
-            <h3
-              class="mt-4 line-clamp-2 text-lg font-bold"
-            >
-
-              {{ item.title }}
-
-            </h3>
-
-            <p
-              class="mt-3 text-sm text-gray-500"
-            >
-
-              <!-- {{ item.time }} -->
-
-            </p>
-
-          </div>
-
-        </article>
-
-      </div>
-
-    </div>
-
-  </UCard>
-
-                </template>
-            </div>
-    
-    
-            <!-- SECTION 5 -->
-    
-    
-            <HoursToLimit :cards="cards" />
-    
-    
-            <div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
-    
-                <!-- SECTION 6 -->
-                <UCard>
-    
-                    <FlightHoursChart />
-    
-                </UCard>
-    
-    
-    
-                <!-- SECTION 7 -->
-    
-                <MyDocuments :documents="documentList" />
-            </div>
+            </UCard>
+            
+            
         </div>
-    
-    
-    
+
+        <!-- SECTION 5 -->
+
+        <HoursToLimit 
+        :cards="cards"
+        />
+
+
+        <div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
+
+            <!-- SECTION 6 -->
+            <UCard>
+
+                <FlightHoursChart />
+
+            </UCard>
+
+
+            <MyDocuments 
+            :documents="documentStore.documentList"
+            />
+        </div>
+    </div>
+
+
 </template>
